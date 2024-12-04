74a9648ebe249aeb55062a7e9900e6d1afcc2d6a
helen-bernal <helen.bernal@globant.com>
2024-12-03 23:56:45 -0500

    corrections

--- src/test/java/utils/MyDriver.java
+++ src/test/java/utils/MyDriver.java
@@ -29,33 +29,27 @@ public class MyDriver {
 
             System.out.println("Ruta del chromedriver: " + driverPath);
 
-            // Usar WebDriverManager o establecer el path si se está usando el driver manualmente
             System.setProperty("webdriver.chrome.driver", driverPath);
 
-            // Configuración de ChromeOptions
             ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless");  // Ejecutar en modo sin cabeza
+            options.addArguments("--headless");
 
-            // Configuración del servicio de ChromeDriver
             ChromeDriverService service = new ChromeDriverService.Builder()
-                    .usingDriverExecutable(new File(driverPath)) // Ruta del chromedriver
-                    .usingAnyFreePort()  // Utiliza cualquier puerto libre
-                    .withTimeout(Duration.ofSeconds(30))  // Timeout de 30 segundos usando Duration
+                    .usingDriverExecutable(new File(driverPath))
+                    .usingAnyFreePort()
+                    .withTimeout(Duration.ofSeconds(30))
                     .build();
 
-            // Instanciación del WebDriver usando el servicio
             driver = new ChromeDriver(service, options);
         }
     }
 
-    // Método para cerrar el driver
     public void closeDriver() {
         if (driver != null) {
             driver.quit();
         }
     }
 
-    // Método para obtener la instancia del driver
     public WebDriver getDriver() {
         return this.driver;
     }
