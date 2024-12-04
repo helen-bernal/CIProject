61c0c34ef973a7384a4a54626f34ee83cc6d627e
helen-bernal <helen.bernal@globant.com>
2024-12-03 20:47:45 -0500

    Adding System.getenv(“variableName”) to the BaseTest

--- src/test/java/utils/baseTest/BaseTest.java
+++ src/test/java/utils/baseTest/BaseTest.java
@@ -11,11 +11,14 @@ import org.openqa.selenium.WebDriver;
 public class BaseTest {
     MyDriver mydriver;
 
-    @BeforeMethod(alwaysRun=true)
+    @BeforeMethod(alwaysRun = true)
     @Parameters({"browser", "url"})
     public void beforeMethod(@Optional("chrome") String browser, @Optional("https://www.saucedemo.com/") String url) {
+        // Prioridad: Línea de comandos -> Parámetro de TestNG -> Predeterminado
+        String browserType = System.getProperty("browserName", browser);
+
         if (mydriver == null || mydriver.getDriver() == null) {
-            mydriver = new MyDriver(browser);
+            mydriver = new MyDriver(browserType);
             mydriver.getDriver().manage().window().maximize();
         }
         navigateTo(url);
@@ -29,11 +32,14 @@ public class BaseTest {
         return new LoginPage(mydriver.getDriver());
     }
 
-    @AfterMethod(alwaysRun=true)
+    @AfterMethod(alwaysRun = true)
     public void afterMethod() {
-        //mydriver.closeDriver();
+        if (mydriver != null && mydriver.getDriver() != null) {
+            mydriver.getDriver().quit(); // Asegura liberar recursos al final de cada test
         }
+    }
+
     public WebDriver getDriver() {
         return mydriver != null ? mydriver.getDriver() : null;
     }
-    }
+}
